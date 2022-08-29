fc8a4f82196158a22e4a278bddf31682662cda8c
Ana Paula Cardozo <ana_paula_cardozo@hotmail.com>
2022-08-29 20:22:52 -0300

    atualizando funcoes

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-function tocaSomPom () {
-  document.querySelectorAll('.tecla').play();
+function tocaSom (idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
-
-document.querySelectorAll('.tecla');
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`; //template string
+
+  tecla.onclick = function () {
+      tocaSom(idAudio);
+  }
+
+}
