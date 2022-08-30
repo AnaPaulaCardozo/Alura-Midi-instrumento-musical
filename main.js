8b8b827a9d55087c536631c551ae9a0abe11583b
Ana Paula Cardozo <ana_paula_cardozo@hotmail.com>
2022-08-29 21:12:22 -0300

    Inserindo funcoes nas teclas

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-function tocaSom (idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+if (elemento && elemento.localName === 'audio' ) {
+  elemento.play();
+}
+else {
+  console.log('Elemento não encontrado ou seletor inválido');
 }
 
+}
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //para
@@ -15,4 +22,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
       tocaSom(idAudio);
   }
 
+  tecla.onkeydown == function (evento) {
+
+    if (evento.code ==='Space' || evento.code ==='Enter') {
+      tecla.classList.add('ativa');
+  }
+
+
+}
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
 }
